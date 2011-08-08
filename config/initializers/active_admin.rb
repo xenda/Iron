6f305f33f6c3cc3ecaa3be1eeadde0ae126df4fa
Alvaro Pereyra <alvaro@xendacentral.com>
2011-08-08 16:50:13 -0500

    Changes how ActiveAdmin works

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,3 +1,15 @@
+if Rails.env.production?
+  require 'fileutils'
+  FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets", "admin"))
+
+  template_path = "#{Gem.loaded_specs['activeadmin'].full_gem_path}/lib/active_admin/stylesheets"
+  old_compile_path = "#{Rails.root}/public/stylesheets/admin"
+  new_compile_path = "#{Rails.root}/tmp/stylesheets/admin"
+
+  Sass::Plugin::remove_template_location template_path, old_compile_path
+  Sass::Plugin::add_template_location template_path, new_compile_path
+end
+
 ActiveAdmin.setup do |config|
 
   # == Site Title
@@ -76,3 +88,4 @@ ActiveAdmin.setup do |config|
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
 end
+
