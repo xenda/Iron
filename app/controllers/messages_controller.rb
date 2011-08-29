7035038fb610a1b14d214a22202643fac9af7825
Alvaro Pereyra <alvaro@xendacentral.com>
2011-08-29 15:05:25 -0500

    Updates controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,15 @@ class MessagesController < InheritedResources::Base
   # before_filter :authenticate_user!
   after_filter :send_to_pusher
   
+  def new
+    @message = Message.new  
+    @message.category_id = 1
+    new!
+  end
+
   def create
+    @message = Message.new(params[:message])
+    @message.category_id = 1
     create! { root_path }
   end
   
